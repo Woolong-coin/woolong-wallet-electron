5c29b3d4fb040de444f74e33fdef292db4439e4e
Woolong-coin <50668946+Woolong-coin@users.noreply.github.com>
2019-05-25 15:42:27 -0700

    Update ws_config.js

--- src/js/ws_config.js
+++ src/js/ws_config.js
@@ -45,7 +45,7 @@ config.remoteNodeListFiltered = true;
 
 // fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
 config.remoteNodeListFallback = [
-  '162.219.245.39:11897',
+  '162.219.245.39:11898',
 ];
 config.remoteNodeCacheSupported = false;
 config.remoteNodeSslSupported = false;
